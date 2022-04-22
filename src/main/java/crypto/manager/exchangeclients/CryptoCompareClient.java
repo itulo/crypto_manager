b09ef17cf29fcb57899eac4e4a564421b40a6280
Italo Armenti <italo@Italos-MacBook-Pro-2.local>
2022-04-22 11:19:10 +0300

    CryptoCompareClient: if reached rate limit, sleep and retry

--- src/main/java/crypto/manager/exchangeclients/CryptoCompareClient.java
+++ src/main/java/crypto/manager/exchangeclients/CryptoCompareClient.java
@@ -1,30 +1,55 @@
 package crypto.manager.exchangeclients;
 
-import org.springframework.http.HttpMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * cryptocompare.com
  * Check value of coins in a specific fiat
  */
 public class CryptoCompareClient {
-
-    private final static String URL = "https://min-api.cryptocompare.com/data/price?fsym={coin}&tsyms=EUR";
-
-    private final static RestTemplate restTemplate = new RestTemplate();
+    private static final Logger log = LoggerFactory.getLogger(CryptoCompareClient.class);
+    private static final String URL = "https://min-api.cryptocompare.com/data/price?fsym={coin}&tsyms=EUR";
+    private static final RestTemplate restTemplate = new RestTemplate();
 
     // TODO make it a time based cache
-    private final static Map<String, Double> coinValueInFiat = new HashMap<>();
+    private final static ConcurrentMap<String, Double> coinValueInFiat = new ConcurrentHashMap<>();
 
     private final static Map<String, String> coinOtherName = Map.of(
-            "iota", "miota"
+            "iota", "miota"     //binance calls it iota, cryptocompare calls it miota
     );
+
+    /**
+     * Process response from CryptoCompare
+     * Even though the response.status is 200, the response body might mention an error
+     * If the error is related to rate limit, then wait and retry
+     *
+     * @param body
+     * @param coin
+     * @return
+     */
+    private static double processResponse(CryptoCompareResponseBody body, String coin){
+        // TODO retry max 3 times instead of trying forever and use backoff to increase sleep time
+        if(body.response() != null && body.message() != null && body.message().contains("rate limit")){
+            log.info("CryptoCompare says we have reached the rate limit, wait and retry!");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                log.error("Could not sleep...");
+            }
+
+            return getCoinValueFromExchange(coin);
+        }
+
+        return body.eur();
+    }
 
     private static double getCoinValueFromExchange(String coin){
         String coinName = coinOtherName.getOrDefault(coin.toLowerCase(), coin);
@@ -32,7 +57,7 @@ public class CryptoCompareClient {
         ResponseEntity<CryptoCompareResponseBody> response
                 = restTemplate.getForEntity(conversionUrl, CryptoCompareResponseBody.class);
         if (response.getStatusCode() == HttpStatus.OK) {
-            return response.getBody().getEur();
+            return processResponse(response.getBody(), coin);
         } else {
             return 0L;
         }
--- src/main/java/crypto/manager/exchangeclients/CryptoCompareResponseBody.java
+++ src/main/java/crypto/manager/exchangeclients/CryptoCompareResponseBody.java
@@ -2,12 +2,9 @@ package crypto.manager.exchangeclients;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class CryptoCompareResponseBody {
-    @JsonProperty("EUR")
-    private double eur;
-
-    public double getEur() {
-        return eur;
-    }
-}
+public record CryptoCompareResponseBody (
+        @JsonProperty("Response") String response,
+        @JsonProperty("Message") String message,
+        @JsonProperty("EUR") double eur)
+{}
 
